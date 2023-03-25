b332f58ca88947054dc80959af78ecfef4c3d46f
Zhoutong Wang <wangzt@amazon.com>
2023-03-26 00:59:53 +0800

    sh shell

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,4 @@
-sudo aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 040045840992.dkr.ecr.ap-southeast-1.amazonaws.com
-sudo docker build -t multitracker .
-sudo docker tag multitracker:latest 040045840992.dkr.ecr.ap-southeast-1.amazonaws.com/multitracker:latest
-sudo docker push 040045840992.dkr.ecr.ap-southeast-1.amazonaws.com/multitracker:latest
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 040045840992.dkr.ecr.ap-southeast-1.amazonaws.com
+docker build -t multitracker .
+docker tag multitracker:latest 040045840992.dkr.ecr.ap-southeast-1.amazonaws.com/multitracker:latest
+docker push 040045840992.dkr.ecr.ap-southeast-1.amazonaws.com/multitracker:latest
